9531731d309ccd4fe40191cec0b9f3ef4a5e50f5
Vitale Petrovich <vitale.petrovich@gmail.com>
2019-01-26 11:50:27 +0300

    step_13_finished

---
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace KatlaSport.Services.HiveManagement
+{
+    /// <summary>
+    /// Represent a validator for <see cref="UpdateHiveSectionRequestValidator"/>
+    /// </summary>
+    class UpdateHiveSectionRequestValidator : AbstractValidator<UpdateHiveSectionRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateHiveSectionRequestValidator"/> class.
+        /// </summary>
+        public UpdateHiveSectionRequestValidator()
+        {
+            RuleFor(r => r.Name).Length(4, 60);
+            RuleFor(r => r.Code).Length(5);
+            RuleFor(r => r.StoreHiveId).GreaterThan(0);
+        }
+    }
+}
